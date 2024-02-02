8938e26c307cb28f74424cac2f282deb96e93547
dgelessus <dgelessus@users.noreply.github.com>
2024-02-02 01:45:35 +0100

    Convert VaultFindNodes callback to std::function and lambdas

--- Sources/Plasma/PubUtilLib/plVault/plVaultClientApi.cpp
+++ Sources/Plasma/PubUtilLib/plVault/plVaultClientApi.cpp
@@ -1755,12 +1755,11 @@ void VaultForceSaveNodeAndWait (
 //============================================================================
 void VaultFindNodes (
     hsWeakRef<NetVaultNode> templateNode,
-    FVaultFindNodeCallback  callback,
-    void *                  param
+    FVaultFindNodeCallback  callback
 ) {
-    NetCliAuthVaultNodeFind(templateNode.Get(), [callback, param](auto result, auto nodeIdCount, auto nodeIds) {
+    NetCliAuthVaultNodeFind(templateNode.Get(), [callback = std::move(callback)](auto result, auto nodeIdCount, auto nodeIds) {
         if (callback) {
-            callback(result, param, nodeIdCount, nodeIds);
+            callback(result, nodeIdCount, nodeIds);
         }
     });  
 }
